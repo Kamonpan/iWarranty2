472aa6de3f1dc18fae0c0a6cd1339b303a20dca8
Jaruwit Sriburanasorn <jaruwits@Jaruwits-MacBook-Pro.local>
2017-08-18 10:23:59 +0700

    ignore pods/

--- Pods/Target Support Files/Pods-MyISProject/Pods-MyISProject-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyISProject : NSObject
-@end
-@implementation PodsDummy_Pods_MyISProject
-@end
--- Pods/Target Support Files/Pods-MyISProjectUITests/Pods-MyISProjectUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MyISProjectUITests : NSObject
-@end
-@implementation PodsDummy_Pods_MyISProjectUITests
-@end
